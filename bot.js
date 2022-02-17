9cc20e4dac4ef1e184f1c73a625540cc598c267b
lordaksik <48729120+lordaksik@users.noreply.github.com>
2022-02-17 10:31:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ bot.hears('/bot', async (ctx) => {
       var total2=0;
       var total_men=0;
       var total_men2=0;
+      var win=0,win2=0;
       for (let i = 0; i <= 29; i++) {
          number = data.items.results[i].results[0].number
          number2 = data.items.results[i].results[1].number
@@ -38,35 +39,49 @@ bot.hears('/bot', async (ctx) => {
          if(number<4){
             total_men++;
          }
-          if(number<4){
+          if(number2<4){
             total_men2++;
          }
          if(number2>3){
             total2++;
          }
-         
+      
       }
       console.log(total+" "+total2+" "+total_men+" "+total_men2)
      if(total===7){
-     ctx.reply("7 бросков на красном больше 3 точек");
+     ctx.reply("7 бросков на КРАСНОМ больше 3 точек");
      }
        if(total2===7){
-     ctx.reply("7 бросков на синем больше 3 точек");
+     ctx.reply("7 бросков на СИНЕМ больше 3 точек");
      }
         if(total_men===7){
-     ctx.reply("7 бросков на синем меньше 4 точек");
+     ctx.reply("7 бросков на КРАСНОМ меньше 4 точек");
      }
         if(total_men2===7){
-     ctx.reply("7 бросков на синем меньше 4 точек");
+     ctx.reply("7 бросков на СИНЕМ меньше 4 точек");
      }
-   
-
+     for (let i = 0; i <= 4; i++) {
+         number = data.items.results[i].results[0].number
+         number2 = data.items.results[i].results[1].number
+         if(number>number2){
+            win++;
+         }
+        if(number<number2){
+            win++;
+         }
+      }
+ if(win===5){
+     ctx.reply("КРАСНЫЙ победил 5 раз подряд");
+     }
+       if(win2===5){
+     ctx.reply("СИНИЙ победил 5 раз подряд");
+     }
    }
    function good() {
       ctx.reply( "Вы запустили Бота на стратегию «Кости» ⚠ Не забудьте поставить особые уведомления на Бота, и ждите сигнала на валуйные ситуации!");
        ctx.reply( "Удачи! По всем вопросам пишите @BetgamesTV_Admin"); 
       ctx.reply('Бот отслежки запущен!')
-      global.time = setInterval(request2, 40000)
+      global.time = setInterval(request2, 45000)
    }
    good()
 }
